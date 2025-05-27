9d128ab0f75ac7a76ec128f1bbf06becc744ec4e
Maarkitoos15 <170023443+Maarkitoos15@users.noreply.github.com>
2025-05-27 16:51:10 +0200

    update()newControllerAuth

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
-const routes = require('./routes');
+const routes = require('./routes'); // rutas generales (productos)
+const registerRoute = require('./routes/register'); // ruta específica de registro
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middlewares
+app.use(cors());
 app.use(express.json());
 
 // Rutas
 app.use('/api', routes);
+app.use('/api', registerRoute); // aquí se añade /api/register
 
-// Listener
 app.listen(PORT, () => {
   console.log(`🚀 Servidor escuchando en http://localhost:${PORT}`);
 });
